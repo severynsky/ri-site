44cabc168bbe0bf9339c4104e44650df3e064c70
severynsky <severynr@gmail.com>
2015-06-06 16:32:01 +0300

    main page ready

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,42 +6,42 @@ $(document).ready(function(){
 
 
 
-// $('.scene').parallax();
-$('.item').click(function(){
-$.ajax({
-    type: 'GET',
-    url: 'http://api.randomuser.me/',
-    dataType: "json",
-    success: function(data){
-    console.log(data.results);
-}
-});
-});
+    // $('.scene').parallax();
+    $('.item').click(function(){
+    $.ajax({
+        type: 'GET',
+        url: 'http://api.randomuser.me/',
+        dataType: "json",
+        success: function(data){
+        console.log(data.results);
+    }
+    });
+    });
 
 
-function showHide(){
-$('.content').fadeIn(200);
-$('.popup').hide();
-$('.fadeout').hide();
-};
+    function showHide(){
+    $('.content').fadeIn(200);
+    $('.popup').hide();
+    $('.fadeout').hide();
+    };
 
-showHide()
+    showHide()
 
-$('.item, .items > div').click(function(){
-  $('.content').fadeOut(200);
-  $('.popup').fadeIn(500);
-  $('.fadeout').fadeIn(500);
-
-
-});
-
-$('.fadeout, .closeBtn').click(function(){
-return showHide();
-});
+    $('.item, .items > div').click(function(){
+      $('.content').fadeOut(200);
+      $('.popup').fadeIn(500);
+      $('.fadeout').fadeIn(500);
 
 
-$( window ).resize(function() {
+    });
+
+    $('.fadeout, .closeBtn').click(function(){
+    return showHide();
+    });
 
 
-});
+    $( window ).resize(function() {
+
+
+    });
 });
