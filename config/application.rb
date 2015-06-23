d6f583d3a3102291d64ddbb97eeba945262babf9
severynsky <severynr@gmail.com>
2015-06-23 15:57:41 +0300

    chng for heroku

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,9 @@ module RotarySite
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_assets = false
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official", "assets", "fonts", "templates", "controllers", "services", "ckeditor")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official", "fonts", "templates", "controllers", "services", "ckeditor", "angular")
+
+    config.assets.js_compressor = Uglifier.new(mangle: false)
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
