5d1bb3f3827c672114ceb6b126db59c08994e4db
severynsky <severynr@gmail.com>
2015-06-23 17:52:22 +0300

    bower isntalled

--- config/application.rb
+++ config/application.rb
@@ -43,5 +43,6 @@ module RotarySite
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
   end
 end
